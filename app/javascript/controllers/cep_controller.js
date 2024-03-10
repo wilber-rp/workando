3ea2bba647d908a4b1048acb1b344351e1a59f61
Marcos Flach <marcosoftn1@gmail.com>
2024-03-09 21:05:00 -0300

    Updated CEP API, in this new API is possible to get lat and long cordenates.

--- app/javascript/controllers/cep_controller.js
+++ app/javascript/controllers/cep_controller.js
@@ -11,12 +11,12 @@ export default class extends Controller {
     const city = document.getElementById('citySelect');
 
     // Consult API to get CEP values
-    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+    fetch(`https://cep.awesomeapi.com.br/json/${cep.value}`)
       .then(response => response.json())
       .then(data => {
-          if (data.erro !== "true" ) {
-            address.value = data.logradouro;
-            city.value = data.localidade;
+          if (data.code !== "not_found" ) {
+            address.value = data.address;
+            city.value = data.city;
           } else {
               address.value = "Cep não encontrado"
               city.value = "Cep não encontrado"
