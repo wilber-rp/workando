98dc3c36a8b99b66d53c5bce83f940f51d0663b0
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-03-11 18:38:42 -0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_based_on_user_type
+
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :dispatch_user
 
