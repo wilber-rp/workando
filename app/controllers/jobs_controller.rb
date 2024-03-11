20449ad82f3a3736827587ab1753790c99e0df96
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-03-11 12:44:46 -0300

    done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,10 @@
 class JobsController < ApplicationController
   def index
-    @jobs = Job.all
+    if current_user == current_user.role_company?
+      @jobs = current_user.job
+    else
+      @jobs = Job.all
+    end
   end
 
   def show
