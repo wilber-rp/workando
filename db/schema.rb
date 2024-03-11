6f79f2decfe9aec2e959ebccd3c9c3130448ae3c
stefano martins <stefano.rcastilho@gmail.com>
2024-03-11 11:42:11 -0300

    11.03.2024

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_182358) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "experience"
     t.index ["user_id"], name: "index_candidates_on_user_id"
   end
 
