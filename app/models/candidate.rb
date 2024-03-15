46596dcb381d72a5813bc329e1f76972ce5d4a63
JosimoLopes <joe.lopes@outlook.com>
2024-03-15 13:19:10 -0400

    img input box

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,7 +2,7 @@ class Candidate < ApplicationRecord
   belongs_to :user
 
   # candidate belogon to user because there is a user_Id in it.
-  has_many :candidate_interest_areas
+  has_many :candidate_interest_areas, dependent: :destroy
   has_many :interest_areas, through: :candidate_interest_areas
   has_many :matches, dependent: :destroy
   has_many :distances
