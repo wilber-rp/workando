a3780ffdf505ae3eb4326f874e44815a39b3cd99
JosimoLopes <joe.lopes@outlook.com>
2024-03-14 22:36:32 -0400

    landing page finilly working

---
+++ app/javascript/controllers/formvalidation_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="formvalidation"
+export default class extends Controller {
+  initialize() {
+    this.element.addEventListener("submit", this.validateForm.bind(this));
+  }
+
+  validateForm(event) {
+    const requiredInputs = this.element.querySelectorAll("input[required]");
+    let isEmpty = false;
+
+    requiredInputs.forEach((input) => {
+      if (input.value.trim() === "") {
+        isEmpty = true;
+      }
+    });
+
+    if (isEmpty) {
+      event.preventDefault(); // Prevent form submission
+      alert("Todos os campos precisam ser preenchidos!."); // You can customize this message
+    }
+  }
+}
